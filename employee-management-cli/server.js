5dd436d5b4dd48c2db14eeaa86fa3692bbd2bd06
Moksh <mumukshujain2466@gmail.com>
2026-02-19 14:29:15 +0530

    added render server

--- employee-management-cli/server.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-
-const PORT = process.env.PORT || 3000;
-
-http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Employee Management System is Running ✅</h1>");
-}).listen(PORT);
