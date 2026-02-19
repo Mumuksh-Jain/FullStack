f9576b71e21379f9553d6769ff41928d3cef3e61
Moksh <mumukshujain2466@gmail.com>
2026-02-19 14:26:43 +0530

    added render server

--- employee-management-cli/server.js
+++ employee-management-cli/server.js
@@ -4,13 +4,5 @@ const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-
-  res.end(`
-    <h1>✅ Employee Management System is Live</h1>
-    <p>This project is deployed successfully on Render.</p>
-    <p><a href="https://github.com/Mumuksh-Jain/FullStack">View GitHub Repo</a></p>
-  `);
-
-}).listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+  res.end("<h1>Employee Management System is Running ✅</h1>");
+}).listen(PORT);
